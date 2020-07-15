eab5ca797e8c6833b32a04fba829d45b3fe2d261
Kevin Guan <kevinguan97@gmail.com>
2020-07-16 11:38:41 +1200

    split front and backend

--- front/src/App.js
+++ front/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import {BrowserRouter, Switch, Route} from "react-router-dom"
+import Header from "./components/layouts/Header"
 import Home from "./components/pages/Home"
-import Login from './auth/Login'
-import Register from './auth/Register'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import './App.scss'
+
+
  
 export default function App() {
     return <>
     <BrowserRouter>
+    <Header />
     <Switch>
        <Route exact path="/" component={Home}/>
        <Route path="/login" component={Login}/>
        <Route path="/register" component={Register}/>
-
     </Switch>
     </BrowserRouter>
     </>
---
+++ front/src/components/layouts/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+//import { Container, Row, Col} from "react-bootstrap"
+
+export default function Header() {
+    return (
+        <div>
+           <Link to="/">MERN Todo</Link>
+        </div>
+    )
+}
